ec4c028617453d8d1b93f11d325fb1ca4a0d1d7f
sriteja somepalli <sriteja somepalli>
2017-11-03 22:57:17 +0100

    Basic JS file with variables and arrays

---
+++ JavaScriptBasics/script.js
@@ -0,0 +1,23 @@
+//var MyScore = 500;
+//var HighScore = 600;
+
+//var greeting = MyScore + HighScore;
+
+var greeting = "";
+
+
+var MyFavourites = ["Kohli", "Dhoni", "Williamson", "ABD"];
+//var i;
+
+greeting += MyFavourites[0] + "<br>";
+greeting += MyFavourites[1] + "<br>";
+greeting += MyFavourites[2] + "<br>";
+greeting += MyFavourites[3] + "<br>";
+
+
+
+
+
+
+
+document.getElementById("test").innerHTML = greeting;
