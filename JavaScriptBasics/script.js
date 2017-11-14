a8ef3afd849371a34f8b784d7c88a1b036449e02
sriteja somepalli <sriteja somepalli>
2017-11-14 15:30:34 +0100

    Prototyping a function

--- JavaScriptBasics/script.js
+++ JavaScriptBasics/script.js
@@ -1,19 +1,36 @@
 
 
-/* Function basics
+//Function basics
 
 
-function increaseHighScore(currentScore){
+/*function increaseHighScore(currentScore){
 
     var newHighScore = currentScore * 2;
     return newHighScore;
 
 
 }
-console.log(increaseHighScore(4));  */
+console.log(increaseHighScore(4)); */
 
-// Functions as objects
+//Prototyping a function
 
+var walk = function(){
+
+    return("He is able to walk..!");
+}
+
+var SuperHero = function(){
+
+    var power,villian;
+}
+
+SuperHero.prototype.walk =walk;
+
+peterParker = new SuperHero;
+peterParker.power = "fly";
+peterParker.walk();
+
+console.log(peterParker.walk());
 
 
 
